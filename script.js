65dd1f27008c68431b9a257b1d7dbae125f53941
Anarikke <anla0008@stud.kea.dk>
2024-09-02 11:33:42 +0200

    del 4

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Dette er en alert
-// alert("Hej, jeg er en alert!");
-
-// // Dette er en Wirte (skriver en tekst på siden)
-// document.write("Hej, jeg er en write");
-
-// Erklære variablen og tildeler værdi til variablen via = tegnet
-let myVariable = "Hej du";
-let otherVariable = "Farvel og tak";
-
-// En variabel kan kun have  1 værdi af gangen
-myVariable = "override";
-
-alert(myVariable);
-alert(otherVariable);
+alert(document.getElementById("myP").innerHTML);
