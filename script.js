05a8335115c1961a1c7b738314670db1095fe021
Anarikke <anla0008@stud.kea.dk>
2024-09-02 11:23:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Dette er en alert
+// alert("Hej, jeg er en alert!");
+
+// // Dette er en Wirte (skriver en tekst på siden)
+// document.write("Hej, jeg er en write");
+
+// Erklære variablen og tildeler værdi til variablen via = tegnet
+let myVariable = "Hej du";
+let otherVariable = "Farvel og tak";
+
+// En variabel kan kun have  1 værdi af gangen
+myVariable = "override";
+
+alert(myVariable);
+alert(otherVariable);
